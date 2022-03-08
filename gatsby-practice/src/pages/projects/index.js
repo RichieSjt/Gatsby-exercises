72aa6325aa9b8843941bea7304a059c4e8299b21
Ricardo Juárez Tepos <45005831+RichieSjt@users.noreply.github.com>
2022-03-08 16:48:00 -0600

    Post thumbnails

--- gatsby-practice/src/pages/projects/index.js
+++ gatsby-practice/src/pages/projects/index.js
@@ -1,4 +1,5 @@
 import { graphql, Link } from "gatsby"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import React from "react"
 import Layout from "../../components/Layout"
 import * as styles from "./projects.module.css"
@@ -19,13 +20,20 @@ const Projects = ({ data }) => {
           {projects.map(project => (
             <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
               <div>
+                <GatsbyImage
+                  image={getImage(project.frontmatter.thumb)}
+                  alt={project.frontmatter.title}
+                />
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
             </Link>
           ))}
         </div>
-      <p>Like what you see? Email me at <a href={`mailto:${contact}`}>{contact}</a> for a quote!</p>
+        <p>
+          Like what you see? Email me at{" "}
+          <a href={`mailto:${contact}`}>{contact}</a> for a quote!
+        </p>
       </div>
     </Layout>
   )
@@ -33,12 +41,19 @@ const Projects = ({ data }) => {
 
 export const query = graphql`
   query ProjectsPage {
-    projects: allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+    projects: allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
       nodes {
         frontmatter {
           slug
           stack
           title
+          thumb {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
         }
         id
       }
